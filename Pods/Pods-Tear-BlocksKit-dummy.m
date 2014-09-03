5fecbbdd74e76d0a6baa45ea719d08e5d88b4b92
zheng.cuizh <zheng.cuizh@gmail.com>
2014-09-03 14:09:57 +0800

    准备提交bitbucket

---
+++ Pods/Pods-Tear-BlocksKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tear_BlocksKit : NSObject
+@end
+@implementation PodsDummy_Pods_Tear_BlocksKit
+@end
---
+++ Pods/Pods-Tear-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tear : NSObject
+@end
+@implementation PodsDummy_Pods_Tear
+@end
